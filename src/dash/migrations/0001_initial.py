e5a1d27d8449c56229f7c346cdfc1f744f9886ae
georgistanev <georgi.stanev@gmail.com>
2015-09-18 14:47:00 +0300

    Change dash to depend on the __latest__ auth migration

--- src/dash/migrations/0001_initial.py
+++ src/dash/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('auth', '0006_require_contenttypes_0002'),
+        ('auth', '__latest__'),
     ]
 
     operations = [
