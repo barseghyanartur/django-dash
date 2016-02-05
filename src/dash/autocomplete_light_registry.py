54aa898d182139cb634b762081f618ea704ca8b7
GMcD <gary.macdonald@projectscapa.com>
2016-02-05 22:13:29 +0000

    Dashboard Plugin Edit Name to Id fix.

--- src/dash/autocomplete_light_registry.py
+++ src/dash/autocomplete_light_registry.py
@@ -3,9 +3,21 @@ import autocomplete_light.shortcuts as al
 from dash.models import DashboardPlugin
 
 """ For Dash Plugins """
-al.register(DashboardPlugin,
-    search_fields = ['^plugin_uid'],
-    attrs = { 'placeholder': 'Select Plugin...', 'data-autocomplete-minimum-characters': 1, },
-    widget_attrs = { 'data-widget-maximum-values': 4, 'class': 'imp-autocomplete', },
-)
+class DashboardPluginAutocomplete(al.AutocompleteModelBase):
+    search_fields = ['^plugin_uid']
+    model = DashboardPlugin
+    choices = DashboardPlugin.objects.all()
 
+    def choices_for_values(self):
+        """
+            Map Plugin_Uid names to Ids 
+        """
+        objs = DashboardPlugin.objects.filter(plugin_uid__in=self.values)
+        ids = self.choices.filter(pk__in=[x.id for x in objs])
+        values = []
+        for value in self.values:
+            values.append(DashboardPlugin.objects.get(plugin_uid=value).id)
+        self.values = values
+        return self.order_choices(ids)
+
+al.register(DashboardPluginAutocomplete)
