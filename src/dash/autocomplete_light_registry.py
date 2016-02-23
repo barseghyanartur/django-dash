816be13a7c6120475e931c9cefa013d045bf794b
GMcD <gary.macdonald@projectscapa.com>
2016-02-23 10:26:51 +0000

    Create and Update for Auto Complete.

--- src/dash/autocomplete_light_registry.py
+++ src/dash/autocomplete_light_registry.py
@@ -4,23 +4,29 @@ from dash.models import DashboardPlugin
 
 """ For Dash Plugins """
 class DashboardPluginAutocomplete(al.AutocompleteModelBase):
-    search_fields = ['^plugin_uid']
-    model = DashboardPlugin
-    choices = DashboardPlugin.objects.all()
-
-    attrs = { 'placeholder' : 'Enter Plugin Format...' }
+    search_fields   = ['^plugin_uid']
+    model           = DashboardPlugin
+    choices         = DashboardPlugin.objects.all()
+    attrs           = { 'placeholder' : 'Enter Plugin Format...' }
 
     def choices_for_values(self):
         """
-            Map Plugin_Uid names to Ids 
+            On the way out, do the normal...
         """
-        objs = DashboardPlugin.objects.filter(plugin_uid__in=self.values)
-        ids = self.choices.filter(pk__in=[x.id for x in objs])
-        values = []
-        for value in self.values:
-            values.append(DashboardPlugin.objects.get(plugin_uid=value).id)
-        self.values = values
-        return self.order_choices(ids)
+        try:
+            if (self.values and isinstance(self.values[0], int)) or (isinstance(self.value, int)):
+               return super(DashboardPluginAutocomplete, self).choices_for_values()
+        except (UnboundLocalError, AttributeError):
+            """
+                Map Plugin_Uid names to Ids, on the way in...
+            """
+            objs = DashboardPlugin.objects.filter(plugin_uid__in=self.values)
+            ids = self.choices.filter(pk__in=[x.id for x in objs])
+            values = []
+            for value in self.values:
+                values.append(DashboardPlugin.objects.get(plugin_uid=value).id)
+            self.values = values
+            return self.order_choices(ids)
 
     def validate_values(self):
         return True
