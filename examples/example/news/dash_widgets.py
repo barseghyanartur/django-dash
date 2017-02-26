27a2dc84502c69893dcee7e1c8e2fb8b942f1872
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-02-26 16:33:37 +0100

    cleanup

--- examples/example/news/dash_widgets.py
+++ examples/example/news/dash_widgets.py
@@ -1,12 +1,16 @@
-__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = 'Copyright (c) 2013 Artur Barseghyan'
-__license__ = 'GPL 2.0/LGPL 2.1'
-__all__ = ('BaseNewsWidget', 'News2x5Widget', 'News4x5Widget')
-
 from django.template.loader import render_to_string
 
 from dash.base import BaseDashboardPluginWidget
 from news.constants import PAGE_URL_PARAM, NUM_POSTS_URL_PARAM
+
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2013-2017 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = (
+    'BaseNewsWidget',
+    'News2x5Widget',
+    'News4x5Widget'
+)
 
 # ***********************************************************************
 # ********************** Base widget for Memo plugin ********************
