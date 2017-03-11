9dd8be44c9345859595968a7066c9ae43720ef87
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-03-11 23:23:18 +0100

    clean up script updated

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -1,6 +1,7 @@
 find . -name "*.pyc" -exec rm -rf {} \;
 find . -name "*.py,cover" -exec rm -rf {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
+find . -name ".cache" -exec rm -rf {} \;
 find . -name "*.orig" -exec rm -rf {} \;
 rm MANIFEST.in~
 rm .hgignore~
