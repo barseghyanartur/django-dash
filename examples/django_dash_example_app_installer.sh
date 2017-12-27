ebf301ec93ecb9edd019d0f13af971e5119baa03
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-12-27 22:00:16 +0100

    update installer

--- examples/django_dash_example_app_installer.sh
+++ examples/django_dash_example_app_installer.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
 wget -O django_dash_example_app_installer.tar.gz https://github.com/barseghyanartur/django-dash/archive/stable.tar.gz
 virtualenv dash
 source dash/bin/activate
 mkdir django_dash_example_app_installer/
 tar -xvf django_dash_example_app_installer.tar.gz -C django_dash_example_app_installer
 cd django_dash_example_app_installer/django-dash-stable/examples/example/
-pip install -r ../requirements/django_1_8.txt
+pip install -r ../requirements.txt
 pip install https://github.com/barseghyanartur/django-dash/archive/stable.tar.gz
 mkdir ../media/
 mkdir ../media/static/
@@ -14,7 +15,7 @@ mkdir ../db/
 mkdir ../logs/
 mkdir ../tmp/
 cp settings/local_settings.example settings/local_settings.py
-./manage.py syncdb --noinput --traceback -v 3
+#./manage.py syncdb --noinput --traceback -v 3
 ./manage.py migrate --noinput
 ./manage.py collectstatic --noinput --traceback -v 3
 ./manage.py news_create_test_data --traceback -v 3
