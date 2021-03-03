84be4d061a072b098a0f9cd0b11b8b5e72923bd7
Artur Barseghyan <artur.barseghyan@gmail.com>
2021-03-03 01:17:28 +0100

    Up make release script

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+./scripts/uninstall.sh
+./scripts/clean_up.sh
 python setup.py register
-python setup.py sdist bdist_wheel upload
+python setup.py sdist bdist_wheel
+twine upload dist/*
